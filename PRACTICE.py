665e3ab5137a6ad64ef850fd625ade74e66fce79
Zeshaninfo <83656755+Zeshaninfo@users.noreply.github.com>
2021-05-05 21:20:34 +0600

    Add files via upload

---
+++ PRACTICE.py
@@ -0,0 +1,10 @@
+#this is my Gay program
+x=input('which colour do you want?') #asking the customer their preference of colour
+if x=="Blue": #if the input is blue in colour then the price would be 650
+    print('price is 650')
+elif x=="Red": 
+    print("price is 590")
+elif x== "Cherry blue":
+    print('price is 990')
+else:
+    print("price is 244")
